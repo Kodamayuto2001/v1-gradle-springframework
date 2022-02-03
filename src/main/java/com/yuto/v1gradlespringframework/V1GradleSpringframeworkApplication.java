d5215acaf3bea3249a3dce375183674ff3f7f98a
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2022-02-03 23:48:44 +0900

    first commit

---
+++ src/main/java/com/yuto/v1gradlespringframework/V1GradleSpringframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.yuto.v1gradlespringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1GradleSpringframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1GradleSpringframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuto/v1gradlespringframework/V1GradleSpringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuto.v1gradlespringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1GradleSpringframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
